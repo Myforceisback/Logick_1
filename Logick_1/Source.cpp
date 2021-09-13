6762f9c2b5db8230cea029106a81f19781c03571
Tupoihucker <64333892+Tupoihucker@users.noreply.github.com>
2021-09-13 22:54:12 +0300

    Update Source.cpp

--- Logick_1/Source.cpp
+++ Logick_1/Source.cpp
@@ -110,58 +110,77 @@ using namespace std;
 	}
 	system("pause");
 }*/
-/*#include<iostream>
-#include<clocale>
+/*#include <iostream>
+#include <string>
 using namespace std;
-int const n=5;
- 
-int main(){
-setlocale(LC_ALL,".1251");
- 
-struct student{
-char fio[20];
-int number;
-float usp[n];};
- 
-student N[n];
-int StudBestcount;
- 
-for(int i=0;i<n;++i){
-    cout<<"Введите фамилию и инициалы студента "<<i<<"-ого списка:"<<endl;
-    cin>>N[i].fio;
-    cout<<"Номер группы студента "<<i<<"-ого списка:"<<endl;
-    if(i==0)cin>>N[i].number;
-    else  cin>>N[i].number;
-    cout<<"Введите оценки студента "<<i<<"-ого списка:"<<endl;
-    if(i==0){
-    for(int j=0;j<n;++j){
-        cout<<"Subject["<<j<<"]:";cin>>N[i].usp[j];
-    if(N[i].usp[j]>0 && N[i].usp[j]<=5)continue;
-    else {--j;cout<<"Пятибалльная система!"<<endl;}}}
-    else
-        for(int j=0;j<n;++j){
-        cout<<"Subject["<<j<<"]:";cin>>N[i].usp[j];
-        if(N[i].usp[j]>0 && N[i].usp[j]<=5)continue;
-    else {--j;cout<<"Пятибалльная система!"<<endl;}}}
-cout<<"Сведения о студентах по возрастанию номера группы"<<endl;
-for(int i=0;i<n-1;++i){
-        int imin=i;
-for(int j=i+1;j<n;++j)
-    if(N[j].number<N[imin].number) imin=j;
-float temp=N[i].number; N[i].number=N[imin].number;N[imin].number=temp;}
-for(int i=0;i<n;++i){
-    cout<<"Фамилия и инициалы студента:"<<N[i].fio<<endl;
-    cout<<"Номер группы студента: "<<N[i].number<<endl;
-    cout<<"Оценки студента: "<<N[i].fio<<":"<<endl;
-    for(int j=0;j<n;++j)
-         cout<<"Subject["<<j<<"]:"<<N[i].usp[j]<<endl;}
-cout<<"\nОтличники:\n"<<endl;
-for(int i=0;i<n;++i)
-for(int j=0;j<n;++j){
-    StudBestcount=0;
-    if(N[i].usp[j]>=4.0){cout<<"Фамилия и инициалы студента-отличника:"<<N[i].fio<<endl;
-                        cout<<"Номер группы студента-отличника:"<<N[i].number<<endl;
-                        j=(n-1);
-                        ++StudBestcount;}}
-    if(StudBestcount==0) cout<<"Студентов-отличников нет!"<<endl;
-}*/
+
+struct STUDENT // структура STUDENT
+{
+	string name;
+	int group;
+	float ses[5];
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int size = 10; // количество студентов
+	STUDENT stud1[size]; // массив 
+	int i, j, n;
+	int count = 0; // счётчик студентов с плохими отметками 
+	// eсли он равен количеству студентов значит 
+	// хорошистов нет
+
+	for (n = 0; n < size; n++) // ввод информации
+	{
+		cout << "Введите фамилию студента " << endl;
+		cin >> stud1[n].name;
+		cout << "Введите номер группы " << endl;
+		cin >> stud1[n].group;
+		cout << "Введите 5 оценок " << endl;
+		for (int i = 0; i < 5; i++)
+			cin >> stud1[n].ses[i];
+	}
+
+	STUDENT stud2[size]; // массив для упорядоченных элементов
+	i = 0, j = 0;
+	int min, imin;
+	min = 1023;
+
+	while (stud2[size - 1].group < 0) {    //заполнение массива упорядоченно
+		for (i = 0; i < size; i++) { //вычисление минимального элемента
+			if (stud1[i].group < min) {
+				min = stud1[i].group;
+				imin = i;
+			}
+		}
+		stud2[j] = stud1[imin]; //перенос элементов
+		stud1[imin].group = 1023;
+		min = 1023;
+		j++;
+	}
+	float sum = 0; // счётчик суммы оценок
+	cout << "Фамилии и номера групп студентов со средней оценкой выше 4.0" << endl;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < 5; j++)
+			sum += stud2[i].ses[j];
+
+		if (sum / 5 > 4.0) // условие хорошиста
+		{
+			cout << "Фамилия: " << stud2[i].name << endl;
+			cout << "Номер группы: " << stud2[i].group << endl;
+		}
+		else
+			count++;
+
+		sum = 0;
+	}
+	if (count == size)
+		cout << "Такие студенты отсутствуют " << endl;
+
+	system("Pause");
+
+	return 0;
+}
+*/
