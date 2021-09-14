b8908b351c26a3748bb2ef2f9afe0763da91f477
Tupoihucker <64333892+Tupoihucker@users.noreply.github.com>
2021-09-14 12:37:29 +0300

    Update Source.cpp

--- Logick_1/Source.cpp
+++ Logick_1/Source.cpp
@@ -1,97 +1,84 @@
+#include "stdafx.h"
 #include <iostream>
 #include <string>
 #include <Windows.h>
 #include <locale.h>
-#include <vector>
+#include <time.h>
 
 using namespace std;
 
-/*void main() {
+
+void zadanie1() {
 	int max, min, n;
 	int mas[10];
 	setlocale(LC_ALL, "Rus");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-
-	cout << "Çàäàíèå 1" << endl;
-
+	cout << "Zadanie 1" << endl;
 	for (int i = 0; i < 10; i++) {
-		cout << "Ââåäèòå ÷èñëî  " << i + 1 << endl;
+		cout << "Vvedite " << i + 1 << " element = " << " ";
 		cin >> n;
 		mas[i] = n;
 	}
 	max = mas[0];
 	min = mas[0];
-
 	for (int i = 1; i < 10; i++) {
 		if (max < mas[i])
 			max = mas[i];
 		if (min > mas[i])
 			min = mas[i];
 	}
-	cout << "Ðàçíèöà = " << max - min << endl;
+	cout << "Raznica = " << max - min << endl;
 	system("pause");
-}*/
+}
 
-/*void main() {
+void zadanie2() {
 	setlocale(LC_ALL, "Rus");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-
-	cout << "Çàäàíèå 2" << endl;
+	cout << "Zadanie 2" << endl;
 	
 	int mas[10];
 	srand(static_cast < unsigned int > (time(NULL)));
-
 	for (int i = 0; i < 10; i++) {
 		mas[i] = rand() % 100;
 		cout << mas[i] << " \n";
 	}
-
 	system("pause");
-}*/
+}
 
-/*void main() {
+void zadanie3() {
 	setlocale(LC_ALL, "Rus");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-
 	int n;
-	cout << "Çàäàíèå 3" << endl;
-	cout << "Ââåäèòå ðàçìåð ìàññèâà = " << " ";
+	cout << "Zadanie 3" << endl;
+	cout << "Vvedite dlinnu massiva = " << " ";
 	cin >> n;
-
 	int* mas = new int[n];
-
 	for (int i = 0; i < n; i++) {
-
-		mas[i] = rand() % 100;
+		mas[i] = 10000 + rand() % (100000-10000);
 		cout << mas[i] << " \n";
 	}
 	delete[] mas;
-}*/
-/*void main() {
-
+	system("pause");
+}
+void zadanie4() {
 	setlocale(LC_ALL, "Rus");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-
-	cout << "Çàäàíèå 4" << endl;
-
+	cout << "Zadanie 4" << endl;
 	int mas_matr[10][10];
 	int n, m, score = 0;
-
-	cout << "Ââåäèòå êîë-âî ñòîëáöîâ = " << " ";
+	cout << "Vvedite kol-vo stolbcov = " << " ";
 	cin >> n;
 	cout << endl;
-
-	cout << "Ââåäèòå êîë-âî ñòðîê = " << " ";
+	cout << "Vvedite kol-vo strok = " << " ";
 	cin >> m;
 	cout << endl;
-
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			cin >> mas_matr[i][j];
+			mas_matr[i][j] = rand() % 1000;
 		}
 	}
 	cout << endl;
@@ -106,26 +93,26 @@ using namespace std;
 		for (int j = 0; j < m; j++) {
 			score += mas_matr[i][j];
 		}
-		cout << "Ñóììà " << i + 1 << " ñòðîêè = " << score << endl;
+		cout << "Summa " << i + 1 << " stroki = " << score << endl;
 	}
 	system("pause");
-}*/
-/*
-int const n = 5;
-int main() {
-	setlocale(LC_ALL, ".1251");
-	setlocale(LC_ALL, "Russian");
+}
 
+void zadanie5() {
+	setlocale(LC_ALL, "Rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int const nn = 5;
+	cout << "Zadanie 5" << endl;
+	
 	struct student {
 		char fio[20];
 		int number;
-		float usp[n];
+		float usp[nn];
 	};
-
-	student N[n];
+	student N[nn];
 	int StudBestcount;
-
-	for (int i = 0; i < n; ++i) {
+	for (int i = 0; i < nn; ++i) {
 		cout << "Введите фамилию и инициалы студента " << i << "-ого списка:" << endl;
 		cin >> N[i].fio;
 		cout << "Номер группы студента " << i << "-ого списка:" << endl;
@@ -133,44 +120,53 @@ int main() {
 		else  cin >> N[i].number;
 		cout << "Введите оценки студента " << i << "-ого списка:" << endl;
 		if (i == 0) {
-			for (int j = 0; j < n; ++j) {
+			for (int j = 0; j < nn; ++j) {
 				cout << "Subject[" << j << "]:"; cin >> N[i].usp[j];
 				if (N[i].usp[j] > 0 && N[i].usp[j] <= 5)continue;
 				else { --j; cout << "Пятибалльная система!" << endl; }
 			}
 		}
 		else
-			for (int j = 0; j < n; ++j) {
+			for (int j = 0; j < nn; ++j) {
 				cout << "Subject[" << j << "]:"; cin >> N[i].usp[j];
 				if (N[i].usp[j] > 0 && N[i].usp[j] <= 5)continue;
 				else { --j; cout << "Пятибалльная система!" << endl; }
 			}
 	}
 	cout << "Сведения о студентах по возрастанию номера группы" << endl;
-	for (int i = 0; i < n - 1; ++i) {
+	for (int i = 0; i < nn - 1; ++i) {
 		int imin = i;
-		for (int j = i + 1; j < n; ++j)
+		for (int j = i + 1; j < nn; ++j)
 			if (N[j].number < N[imin].number) imin = j;
 		float temp = N[i].number; N[i].number = N[imin].number; N[imin].number = temp;
 	}
-	for (int i = 0; i < n; ++i) {
+	for (int i = 0; i < nn; ++i) {
 		cout << "Фамилия и инициалы студента:" << N[i].fio << endl;
 		cout << "Номер группы студента: " << N[i].number << endl;
 		cout << "Оценки студента: " << N[i].fio << ":" << endl;
-		for (int j = 0; j < n; ++j)
+		for (int j = 0; j < nn; ++j)
 			cout << "Subject[" << j << "]:" << N[i].usp[j] << endl;
 	}
 	cout << "\nОтличники:\n" << endl;
-	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < n; ++j) {
+	for (int i = 0; i < nn; ++i)
+		for (int j = 0; j < nn; ++j) {
 			StudBestcount = 0;
 			if (N[i].usp[j] >= 4.0) {
 				cout << "Фамилия и инициалы студента-отличника:" << N[i].fio << endl;
 				cout << "Номер группы студента-отличника:" << N[i].number << endl;
-				j = (n - 1);
+				j = (nn - 1);
 				++StudBestcount;
 			}
 		}
 	if (StudBestcount == 0) cout << "Студентов-отличников нет!" << endl;
+	system("pause");
 }
-*/
+
+void main(){
+	zadanie1();
+	zadanie2();
+	zadanie3();
+	zadanie4();
+	zadanie5();
+	system("pause");
+}
